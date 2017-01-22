94df2eb749b20f53a0238b18fa127fd2f6fe228e
Eric Gonzalez <eric@plasticgnome.com>
2017-01-21 22:49:12 -0800

    Use Closest Pole for ROI Calculations

--- ros/src/traffic_light_classifier/traffic_light_classifier.py
+++ ros/src/traffic_light_classifier/traffic_light_classifier.py
@@ -15,13 +15,13 @@ from keras.preprocessing.image import img_to_array
 from road_wizard.msg import Signals
 from runtime_manager.msg import traffic_light
 from sensor_msgs.msg import Image
+from geometry_msgs.msg import PoseStamped
 from utils import image_utils
 
 IMAGE_DIMENSIONS = (96, 64)
 
 
 class TrafficClassifier:
-
   model = None
   model_path = None
   last_published_prediction = Light.UNKNOWN
@@ -39,10 +39,11 @@ class TrafficClassifier:
 
     roi_signal = message_filters.Subscriber('roi_signal', Signals)
     camera_image = message_filters.Subscriber('image_raw', Image)
+    current_pose = message_filters.Subscriber('current_pose', PoseStamped)
     self.light_detected_publisher = rospy.Publisher('light_color', traffic_light, queue_size=1)
     self.roi_image = rospy.Publisher('roi_image', Image, queue_size=1)
 
-    time_sync = message_filters.ApproximateTimeSynchronizer([roi_signal, camera_image], 5, .1)
+    time_sync = message_filters.ApproximateTimeSynchronizer([roi_signal, camera_image, current_pose], 5, .1)
     time_sync.registerCallback(self.detect_signal)
 
   def predict_light(self, cropped_roi):
@@ -71,7 +72,7 @@ class TrafficClassifier:
       self.model = load_model(self.model_path)
     return self.model
 
-  def detect_signal(self, signal, image):
+  def detect_signal(self, signal, image, current_pose):
     if len(signal.Signals) == 0:
       # No signals are visible
       self.publish_prediction(Light.UNKNOWN)
@@ -82,7 +83,7 @@ class TrafficClassifier:
     cv_image = cv_bridge.imgmsg_to_cv2(image, "rgb8")
     image = PIL.Image.fromarray(cv_image)
 
-    cropped_roi = image_utils.crop_signal(signal, image)
+    cropped_roi = image_utils.crop_signal(signal, image, current_pose)
 
     self.roi_image.publish(cv_bridge.cv2_to_imgmsg(numpy.array(cropped_roi), "rgb8"))
     # Run the cropped image through the NN
--- ros/src/traffic_light_classifier/utils/image_utils.py
+++ ros/src/traffic_light_classifier/utils/image_utils.py
@@ -1,12 +1,41 @@
 import sys
+from collections import defaultdict
+
+import rospy
 
 from PIL import Image
+from itertools import groupby
+
 from road_wizard.msg import Signals
 from sensor_msgs.msg import Image
 
-
 RADIUS_MULTIPLIER = 6
 MAX_RADIUS = 10
+
+
+def point_distance_squared(current_position, signal_position):
+  x = current_position.pose.position.x - signal_position.x
+  y = current_position.pose.position.y - signal_position.y
+  z = current_position.pose.position.z - signal_position.z
+  return x * x + y * y + z * z
+
+
+def find_nearest_signal(signals, current_pose):
+  signal_groups = defaultdict(list)
+  distances = {}
+
+  # Group the signals based on pole id
+  for signal in signals.Signals:
+    signal_groups[signal.plId].append(signal)
+
+  # Find the group of poles closest to the current position
+  for key, group in signal_groups.iteritems():
+    first_signal = group[0]
+    distances[first_signal.plId] = point_distance_squared(current_pose, first_signal)
+
+  closest_pole_id = min(distances, key=distances.get)
+  return signal_groups[closest_pole_id]
+
 
 def calculate_bounds(signal):
   xmin = sys.maxint
@@ -15,10 +44,10 @@ def calculate_bounds(signal):
   ymax = -sys.maxint - 1
   radius_max = 0
 
-  for signal in signal.Signals:
-    x = signal.u
-    y = signal.v
-    radius_max = max(radius_max, signal.radius)
+  for light in signal:
+    x = light.u
+    y = light.v
+    radius_max = max(radius_max, light.radius)
     xmin = min(xmin, x)
     xmax = max(xmax, x)
     ymin = min(ymin, y)
@@ -34,7 +63,10 @@ def calculate_bounds(signal):
 def crop_image(image, xmin, xmax, ymin, ymax):
   return image.crop((xmin, ymin, xmax, ymax))
 
-def crop_signal(signal, image):
+
+def crop_signal(signals, image, current_pose):
+  signal = find_nearest_signal(signals, current_pose)
+
   # Find the bounds of the signal
   xmin, xmax, ymin, ymax = calculate_bounds(signal)
 
